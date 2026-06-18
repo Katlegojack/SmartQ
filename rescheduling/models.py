732d382f45f1a5c19464f8790809010576b3a348
Katlego <mmakokat@gmail.com>
2026-06-18 21:43:59 +0200

    Day16 RescheduleOption migration

--- rescheduling/models.py
+++ rescheduling/models.py
@@ -35,4 +35,20 @@ class RescheduleRecommendation(models.Model):
         return f"{self.booking.user.username} - {self.old_booking_date} to {self.suggested_booking_date} - {self.status}"
     
 
+class RescheduleOption(models.Model):
+    recommendation = models.ForeignKey('rescheduling.RescheduleRecommendation',on_delete=models.CASCADE,related_name='option')
+    option_date = models.DateField()
+    option_time = models.TextField()
+    capacity = models.PositiveIntegerField(default=1)
+    booked_count =models.PositiveIntegerField(default=0)
+    available_count = models.PositiveIntegerField(default=1)
+    is_recommended = models.BooleanField(default=False)
+    is_selected = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['option_date','option_time']
+        unique_together = ('recommendation','option_date','option_time')
+    
+    def __str__(self):
+         return f"{self.recommendation.booking.user.username} - {self.option_date} - {self.option_time}"
 
