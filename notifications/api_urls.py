0fe764343d4ef0239f65dcf69d22f0d3f6af39eb
Katlego <mmakokat@gmail.com>
2026-06-22 14:02:56 +0200

    Add mark notification as read API endpoint

--- notifications/api_urls.py
+++ notifications/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path #let us define the url routes
 #Import the API view that returns the logged-in user's notification
-from .api_views import NotificationIsAPIView,UnreadNotificationCountAPIView
+from .api_views import NotificationIsAPIView,UnreadNotificationCountAPIView,MarkNotificationReadAPIView
 
 # These are the notification API routes.
 urlpatterns = [
@@ -9,4 +9,8 @@ urlpatterns = [
 
     #Return the logged in user's unread notification count. FULL URL: /api/v1/notifications/unread-count
     path('unread-count/',UnreadNotificationCountAPIView.as_view(),name='api_unread_notification_count'),
+
+    #Marks 1 notification as read
+    #FULL URL example: /api/v1/notifications/1/mark-read/
+    path('<int:notification_id>/mark-read/',MarkNotificationReadAPIView.as_view(), name='api_mark_notification_read'),
 ]
