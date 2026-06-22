ebed2e86fd46490e230a3fb3f9202090a35e563e
Katlego <mmakokat@gmail.com>
2026-06-22 12:41:35 +0200

    Day21: Add unread notification count API endpoint

--- notifications/api_urls.py
+++ notifications/api_urls.py
@@ -1,9 +1,12 @@
 from django.urls import path #let us define the url routes
 #Import the API view that returns the logged-in user's notification
-from .api_views import NotificationIsAPIView
+from .api_views import NotificationIsAPIView,UnreadNotificationCountAPIView
 
 # These are the notification API routes.
 urlpatterns = [
     #Empty path means this view runs at the parent URL. Ex later: /api/v1/notifications
     path('',NotificationIsAPIView.as_view(),name='api_notification_list'),
+
+    #Return the logged in user's unread notification count. FULL URL: /api/v1/notifications/unread-count
+    path('unread-count/',UnreadNotificationCountAPIView.as_view(),name='api_unread_notification_count'),
 ]
