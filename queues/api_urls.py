96f298e1b6829c725d0924c8ee879274176006c7
Katlego <mmakokat@gmail.com>
2026-07-04 23:01:06 +0200

    Day 27: Mark as no show, Mark as Complete APIs

--- queues/api_urls.py
+++ queues/api_urls.py
@@ -1,9 +1,11 @@
 #Define URL routes
 from django.urls import path
 #Import queue API views
-from queues.api_views import CallNextTicketAPIView
+from queues.api_views import CallNextTicketAPIView,CompleteCurrentTicketAPIView,NoShowCurrentTicketAPIView
 
 
 urlpatterns = [
     path('counters/<int:counter_id>/call-next/',CallNextTicketAPIView.as_view(),name='api_call_next_ticket'),
+    path('counters/<int:counter_id>/complete/',CompleteCurrentTicketAPIView.as_view(),name='api_complete_current_ticket'),
+    path('counters/<int:counter_id>/no-show/',NoShowCurrentTicketAPIView.as_view(),name='api_no_show_current_ticket'),
 ]
--- queues/api_views.py
+++ queues/api_views.py
@@ -11,7 +11,7 @@ from django.shortcuts import get_object_or_404
 #Counter models
 from counters.models import Counter
 #QUeue business logic
-from .services import call_next_ticket
+from .services import call_next_ticket,complete_current_ticket,mark_current_ticket_no_show
 from .serializers import QueueTicketSerializer
 
 class CallNextTicketAPIView(APIView):
@@ -40,4 +40,47 @@ class CallNextTicketAPIView(APIView):
             status = status.HTTP_200_OK
         )
     
+class CompleteCurrentTicketAPIView(APIView):
+    #Marks the customer currently being served at a counter as complete
+    #Only authenticated users may complete this ticket
+    permission_classes = [IsAuthenticated]
+    def post(self,request,counter_id):
+        #Retrieve the requested counter
+        counter = get_object_or_404(Counter,pk=counter_id)
+        #Complete the ticket currenlty assigned to this counter
+        ticket = complete_current_ticket(counter)
+        
+        #if the counter is not serving anyone anymore
+        if ticket is None:
+            return Response(
+                {'detail':'This counter is not serving any customer'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        #Convert the updated ticket into JSON
+        serializer =QueueTicketSerializer(ticket)
+
+        #Return the completed ticket
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
+class NoShowCurrentTicketAPIView(APIView):
+    #Marks customer currently being served at a certain counter as no show
+    #Only authenticated users
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request,counter_id):
+        #Retrieve the requested counter
+        counter = get_object_or_404(Counter,pk=counter_id)
+        #Mark current ticket as no show
+        ticket = mark_current_ticket_no_show(counter)
+        #If the ticket is serving no one
+        if ticket is None:
+            return Response(
+                {'detail':'This counter is not serving any customer'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        #Convert the updated ticket into a JSON
+        serializer = QueueTicketSerializer(ticket)
+        #Return the updated ticket
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+        
